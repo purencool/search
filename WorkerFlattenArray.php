cee244ecd07fc564af8de55871a220808ab4a388
purencool <purencool@gmail.com>
2020-07-31 13:27:50 +1000

    The  architecture was completely wrong so refactored before the library became unmanageable

---
+++ WorkerFlattenArray.php
@@ -0,0 +1,39 @@
+<?php
+
+
+namespace Purencool\Search;
+
+/**
+ *  Search
+ *
+ * @author Purencool
+ * @license GPLV3
+ * @package Purencool\Search
+ *
+ */
+class WorkerSearchStringFinder
+{
+
+  /**
+   * @param $request
+   * @param $search
+   * @param string $type
+   * @return string
+   *
+   */
+  public static function find($request, $search, $type = 'partial') : string
+  {
+
+    if(is_array($search)){
+      return '';
+    } elseif (stripos($search,$request) !== '' && $type == 'partial'){
+      return $request;
+    } elseif(strpos($search,$request) !== '' && $type == 'absolute'){
+      return $request;
+    }
+
+    return '';
+  }
+
+
+}
